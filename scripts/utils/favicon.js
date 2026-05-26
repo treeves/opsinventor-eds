120f545c607e67709e31f001bb3629a08a3bba74
Tad Reeves <tadreeves@gmail.com>
2026-05-26 18:13:35 -0400

    Changed Favicon to `TAD`

--- scripts/utils/favicon.js
+++ scripts/utils/favicon.js
@@ -5,11 +5,13 @@ import { getConfig, getMetadata } from '../ak.js';
   const name = getMetadata('favicon') || 'favicon';
   const favBase = `${codeBase}/img/favicons/${name}`;
 
-  const tags = `<link rel="icon" href="${favBase}.svg" type="image/svg+xml">
-                <link rel="apple-touch-icon" href="${favBase}-180.png">
+  const tags = `<link rel="apple-touch-icon" href="${favBase}-180.png">
                 <link rel="manifest" href="${favBase}.webmanifest">`;
 
   document.head.insertAdjacentHTML('beforeend', tags);
-  const favicon = document.head.querySelector('link[rel="icon"]');
-  if (favicon) favicon.href = `${favBase}.ico`;
+  const favicon = document.head.querySelector('link[rel="icon"]') || document.createElement('link');
+  favicon.rel = 'icon';
+  favicon.type = 'image/svg+xml';
+  favicon.href = `${favBase}.svg`;
+  if (!favicon.parentElement) document.head.append(favicon);
 }());
